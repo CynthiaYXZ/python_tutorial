8d99121e8a22db538d9c7f4bcc8e5ec40330a5c3
Cynthia <Cynthiawijy@gmail.com>
2023-04-17 23:57:21 +0800

    adjust space

--- Covariance_1.py
+++ Covariance_1.py
@@ -2,8 +2,6 @@
 # Covariance for Python program
 #
 import statistics, math
-
-
 def my_covariance(input_x, input_y):
     length = len(input_x)
     cov = 0 
@@ -16,15 +14,12 @@ def my_covariance(input_x, input_y):
         
     cov = cov/length
     return cov
-
 # 1. Input
 input_x = [10,20,30]
 input_y = [12,21,33]
-
 # 2. Process
 answer = my_covariance(input_x, input_y)
 answer = round(answer, 2)
-
 # 3. Output
 print(f"Input X: {input_x}", f"Input Y: {input_y}")
 print(f"Mean X: {statistics.mean(input_x)}", f"Mean Y: {statistics.mean(input_y)}")
