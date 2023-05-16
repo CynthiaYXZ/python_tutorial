0e6980636083741f5db15cecbcca74a47b15fbc1
Cynthia <Cynthiawijy@gmail.com>
2023-05-16 15:25:23 +0800

    spell checker without tokenappend

--- Spacy_1 spellchecker.py
+++ Spacy_1 spellchecker.py
@@ -13,7 +13,7 @@ words = doc.text.split()
 
 # find misspelled words
 misspelled = spell.unknown(words)
-
+rightspelled=spell.known(words)
 # print the misspelled words
 print("Misspelled words:")
 
@@ -30,7 +30,7 @@ print("\nCorrected text:")
 print(" ".join(corrected))
 print(f"Typo:{counter}")
 print(f"{misspelled}")
-
+print(f"{rightspelled}")
 #Regarding the order of print(f"{misspelled}"), 
 #the order of the items returned by the spell.unknown() method is not guaranteed to be consistent,
 #as it depends on the order of the internal data structures used by the SpellChecker class. 
